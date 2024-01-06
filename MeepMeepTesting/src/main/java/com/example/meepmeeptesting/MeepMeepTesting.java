ae4e7bfe2cc2545882a515728daa1342b7484427
AS1624 <thebest1dudes@gmail.com>
2024-01-05 18:35:04 -0900

    add paths.txt, added all red paths up until localization with april tags, began work on blue right path in MeepMeepTesting.java

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -9,22 +10,25 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
+
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(41, 41, Math.toRadians(141.1), Math.toRadians(141.1), 13.21)
                 .setDimensions(14.25, 14)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-34, -56, Math.toRadians(90) ) )
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(-35, 63, Math.toRadians(270) ) )
+                                .splineTo(new Vector2d(-29, 36), Math.toRadians(315))
+                                .strafeLeft(10)
+                                .splineTo(new Vector2d(-56, 36), Math.toRadians(180))
+                                .addDisplacementMarker(() -> {
+                                    /* grab a white pixel */
+                                })
+                                .lineTo(new Vector2d(-56, 12))
+                                .turn(Math.toRadians(-90))
+                                .lineTo(new Vector2d(48, 12))
+                                .strafeRight(24)
                                 .build()
-                        );
+                );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
