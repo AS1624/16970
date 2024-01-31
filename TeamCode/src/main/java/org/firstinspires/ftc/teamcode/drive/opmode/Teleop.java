fc5ef13ee827759b2f84448f76b51e6a693fcf56
Replit user <>
2024-01-31 22:21:46 +0000

    change servo code to work with new design

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Teleop.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2021 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -37,40 +8,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/*
- * This file contains an example of a Linear "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When a selection is made from the menu, the corresponding OpMode is executed.
- *
- * This particular OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
- * This code will work with either a Mecanum-Drive or an X-Drive train.
- * Both of these drives are illustrated at https://gm0.org/en/latest/docs/robot-design/drivetrains/holonomic.html
- * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
- *
- * Also note that it is critical to set the correct rotation direction for each motor.  See details below.
- *
- * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
- * Each motion axis is controlled by one Joystick axis.
- *
- * 1) drive:    Driving forward and backward               Left-joystick Forward/Backward
- * 2) strafe:  Strafing right and left                     Left-joystick Right and Left
- * 3) turn:      Rotating Clockwise and counter clockwise    Right-joystick Right and Left
- *
- * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
- * When you first test your robot, if it moves backward when you push the left stick forward, then you must flip
- * the direction of all 4 motors (see code below).
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
-
 @TeleOp(name="Teleop", group="Linear OpMode")
 
 public class Teleop extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime      = new ElapsedTime();
+  
     private DcMotor leftFrontDrive   = null;
     private DcMotor leftBackDrive    = null;
     private DcMotor rightFrontDrive  = null;
@@ -78,7 +22,7 @@ public class Teleop extends LinearOpMode {
 
     private Servo flipDoor           = null;
     private Servo slideDoor          = null;
-    private Servo belt               = null;
+    private CRServo belt             = null;
     private Servo launcher           = null;
     private Servo lever              = null;
     
@@ -104,7 +48,7 @@ public class Teleop extends LinearOpMode {
         
         flipDoor   = hardwareMap.get(Servo.class, "servo5");
         slideDoor  = hardwareMap.get(Servo.class, "servo4");
-        belt       = hardwareMap.get(Servo.class, "servo2");
+        belt       = hardwareMap.get(CRServo.class, "servo2");
         door2Right = hardwareMap.get(Servo.class, "servo3");
         launcher   = hardwareMap.get(Servo.class, "servo1");
         lever      = hardwareMap.get(Servo.class, "servo0");
@@ -121,56 +65,39 @@ public class Teleop extends LinearOpMode {
         waitForStart();
         runtime.reset();
         
-        boolean trigger1Down = false;
-        boolean trigger2Down = false;
-        double door1Position = DOWN;
-        double door2Position = DOWN;
+        boolean linkTrigger = false;
+        boolean flipTrigger = false;
+        double linkPosition = DOWN;
+        double flipPosition = DOWN;
         
         
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             double max;
             
-            boolean door2Set =   gamepad1.left_bumper;
-            boolean door1Set =   gamepad1.right_bumper;
-            //boolean doorUp   =   gamepad1.y;
-            //boolean doorDown =   gamepad1.x;
+            boolean flipSet         = gamepad1.left_bumper;
+            boolean linkSet         = gamepad1.right_bumper;
             boolean launcherTrigger = gamepad1.y;
+            boolean runBelt         = gamepad2.a;
 
             //arm winch
-            if(door1Set && !trigger1Down){
+            if(linkSet && !linkTrigger){
                 //trigger1Down  = true;
-                door1Position = (door1Position == UP?DOWN:UP);
+                linkPosition = (linkPosition == UP?DOWN:UP);
             }
-            trigger1Down = door1Set;
+            linkTrigger = linkSet;
             
-            if(door2Set && !trigger2Down){
+            if(door2Set && !flipTrigger){
                 //trigger1Down  = true;
-                door2Position = (door2Position == UP?DOWN:UP);
+                flipPosition = (flipPosition == UP?DOWN:UP);
             }
-            trigger2Down = door2Set;
+            flipTrigger = flipSet;
             
             if(launch != launcherTrigger){
                 launch = ! launch;
                 launcherTrigger = ! launcherTrigger;
             }
             
-            if(door1Position > UP){
-                door1Position = UP;
-            }
-            
-            if(door1Position < DOWN){
-                door1Position = DOWN;
-            }
-            
-            
-            if(door2Position > UP){
-                door2Position = UP;
-            }
-            
-            if(door2Position < DOWN){
-                door2Position = DOWN;
-            }
 
             if(DriveMode == RobotCentric){
               double  drive    = - gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
@@ -218,10 +145,8 @@ public class Teleop extends LinearOpMode {
             leftBackDrive.setPower(leftBackPower);
             rightBackDrive.setPower(rightBackPower);
             
-            slideDoor.setPosition(door1Position);
-            flipDoor.setPosition(- door1Position + UP);
-            door2Right.setPosition(door2Position);
-            belt.setPosition(- door2Position + UP);
+            slideDoor.setPosition(slidePosition);
+            belt.setPower(runBelt?1:0);
             launcher.setPosition(launch?0:0.5);
             lever.setPosition(0.9);
             
@@ -230,7 +155,7 @@ public class Teleop extends LinearOpMode {
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
-            telemetry.addData("left bumper", "%1b", door1Set);
+            telemetry.addData("left bumper", "%1b", linkSet);
             telemetry.update();
         }
     }}
